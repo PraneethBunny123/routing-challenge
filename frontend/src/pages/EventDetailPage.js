08bae4a40fbfee5ab10cf655ff53326f8a0fd432
praneeth <gp.gunturu@gmail.com>
2025-04-08 18:56:31 -0500

    adding dummy events in the EventsPage and providing links of them. Each link will navigate to the respective EventDetaiPage with their id in the path

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,5 +1,13 @@
+import { useParams } from "react-router-dom"
+
 export default function EventDetailPage() {
+    const params = useParams()
+
     return (
-        <h1>Event Detail Page</h1>
+        <>
+            <h1>Event Detail Page</h1>
+            <h3>{params.id}</h3>
+        </>
+
     )
 }
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom"
+
+const EVENTS = [
+    {
+      "id": "e1",
+      "title": "A dummy event 1",
+    },
+    {
+        "id": "e2",
+        "title": "A dummy event 2"
+    },
+    {
+        "id": "e3",
+        "title": "A dummy event 3"
+    }
+  ]
+
 export default function EventsPage() {
     return (
-        <h1>Events page</h1>
+        <>
+            <ul>
+                {EVENTS.map(event => (
+                    <li key={event.id}>
+                        <h3>
+                            <Link to={`/events/${event.id}`}>{`${event.title} with id: ${event.id}`}</Link>
+                        </h3>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
