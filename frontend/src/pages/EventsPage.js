70ca2ed1e7d225ecead56de5380d3aa5cd6f6a7d
praneeth <gp.gunturu@gmail.com>
2025-04-11 13:43:44 -0500

    Utilizing the useLoader hook to just return the response and not resData. The loader automatically extracts Response object Promise and transforms it internally. Here the data is retrieved from the useLoaderData() and from that we can get events array from that

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -38,7 +38,8 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData()
+  const data = useLoaderData()
+  const events = data.events
 
   return (
     <>
@@ -56,7 +57,6 @@ export async function loader() {
     //
   }
   else {
-    const resData = await response.json()
-    return resData.events
+    return response
   }
 }
