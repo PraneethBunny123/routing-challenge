a5028af48908a9045d1c6a7e256f8b5e069c2ff5
praneeth <gp.gunturu@gmail.com>
2025-04-12 12:55:34 -0500

    returning back to events page when the form is submitted. For this used the redirect provided by react-router-dom

--- frontend/src/pages/NewEventPage.js
+++ frontend/src/pages/NewEventPage.js
@@ -1,4 +1,4 @@
-import { json } from "react-router-dom";
+import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 export default function NewEventPage() {
@@ -28,4 +28,6 @@ export async function action({request, params}) {
     if(!response.ok) {
         throw json({message: 'could not save data'}, {status: 500})
     }
+
+    return redirect('/events')
 }
