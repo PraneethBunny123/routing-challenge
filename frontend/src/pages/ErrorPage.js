c1d8d51120c5902e46177d18040c01bbef854e41
praneeth <gp.gunturu@gmail.com>
2025-04-11 19:10:28 -0500

    throwing new Response instead of object, so we can send an object, status code in that Response. accessing that Response inside the ErrorPage using useRouteError hook. By using that status code we can give custom error on the screen.

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,9 +1,24 @@
 import PageContent from "../components/PageContent"
+import { useRouteError } from "react-router-dom"
 
 export default function ErrorPage() {
+    const error = useRouteError()
+
+    let title = 'An error occured'
+    let message = 'Something went wrong'
+
+    if(error.status === 500) {
+        message = JSON.parse(error.data).message
+    }
+
+    if(error.status === 404) {
+        title = 'Not found'
+        message = 'Could not find the page'
+    }
+
     return (
-        <PageContent title='An error occured'>
-            <p>Something went wrong</p>
+        <PageContent title={title}>
+            <p>{message}</p>
         </PageContent>
     )
 }
