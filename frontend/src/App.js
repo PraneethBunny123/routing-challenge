8cf6f73e898bbe99a7b6f801bde14e79776fc402
praneeth <gp.gunturu@gmail.com>
2025-04-11 23:07:18 -0500

    making loader: eventDetailLoader as the loader to both EventDetailPage and EditEventPage as making them both as nested inside creating a new object. That way we can use the same loader for both. To use loader like this we need to use useRouteLoaderData hook instead of useLoaderData and we access that using the id that we provide in the App nested path. Also provide the event data to the EventForm as defaultValue to the input feilds. So, the form feilds get the previous data populated so we can edit them in future

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,12 +47,15 @@ const router = createBrowserRouter([
             loader: loaderFunction
           },
           {
-            path: ':id', 
-            element: <EventDetailPage />, 
-            loader: eventDetailLoader
+            path: ':id',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {index: true, element: <EventDetailPage />},
+              {path: 'edit', element: <EditEventPage />}
+            ]
           },
-          {path: 'new', element: <NewEventPage />},
-          {path: ':id/edit', element: <EditEventPage />}
+          {path: 'new', element: <NewEventPage />}
         ]
       }
       
--- frontend/src/pages/EditEventPage.js
+++ frontend/src/pages/EditEventPage.js
@@ -1,5 +1,10 @@
+import { useRouteLoaderData } from "react-router-dom"
+import EventForm from "../components/EventForm"
+
 export default function EditEventPage() {
+    const data = useRouteLoaderData('event-detail')
+
     return (
-        <h1>Edit event page</h1>
+        <EventForm event={data.event}/>
     )
 }
